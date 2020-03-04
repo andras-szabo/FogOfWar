f35c00e0dbc7fcf56b1f96bac63affebe9446b17
Andras Szabo <a.szabo@play-magic.net>
2020-03-04 17:00:14 +0100

    Starting to set up super basic car controls

---
+++ Assets/Scripts/ThirdPersonCar/CarSteering.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CarSteering : MonoWithCachedTransform
+{
+    public Transform frontLeftWheel;
+    public Transform frontRightWheel;
+
+    private NavMeshAgent agent;
+    private float lastTargetTimestamp;
+
+    private Vector3 previousSteeringTarget;
+    private Quaternion targetWheelRotation;
+    private Quaternion startingWheelRotation;
+    private float elapsedInWheelRotation;
+    private float wheelRotationDuration;
+    private float cosMaxTurnAngle;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cosMaxTurnAngle = Mathf.Cos(45F * Mathf.Deg2Rad);
+    }
+
+    private void Update()
+    {
+        if (ControlByMouse.LastTargetSetTimestamp > lastTargetTimestamp)
+        {
+            lastTargetTimestamp = ControlByMouse.LastTargetSetTimestamp;
+            agent.SetDestination(ControlByMouse.TargetPosition);
+        }
+
+        if (agent.velocity.sqrMagnitude > 0f)
+        {
+            var steeringTarget = agent.steeringTarget;
+
+            if (steeringTarget != previousSteeringTarget)
+            {
+                steeringTarget.y = frontLeftWheel.position.y;
+                startingWheelRotation = frontLeftWheel.rotation;
+                Vector3 toSteeringTarget = (steeringTarget - transform.position).normalized;
+
+                if (Vector3.Dot(toSteeringTarget, new Vector3(CachedTransform.forward.x, 0f, CachedTransform.forward.z).normalized) < cosMaxTurnAngle)
+                {
+                    // Check cross product to see if right or left turn
+                    // then apply (sina, cosa) or (-sina, cosa) as toSteeringTarget
+                }
+
+                targetWheelRotation = Quaternion.LookRotation(toSteeringTarget, Vector3.up) * Quaternion.Euler(0f, 0f, 90f);
+                elapsedInWheelRotation = 0f;
+                wheelRotationDuration = Vector3.Angle(frontLeftWheel.forward, toSteeringTarget) / (agent.angularSpeed / 8f);
+
+                previousSteeringTarget = steeringTarget;
+            }
+
+            elapsedInWheelRotation += Time.deltaTime;
+
+            Quaternion wheelRotation = Quaternion.Lerp(startingWheelRotation, targetWheelRotation, 
+                                                       elapsedInWheelRotation / wheelRotationDuration);
+
+            frontLeftWheel.rotation = wheelRotation;
+            frontRightWheel.rotation = wheelRotation;
+        }
+    }
+
+    private Color GetNavAgentStatusColor()
+    {
+        if (agent == null)
+        {
+            return Color.black;
+        }
+
+        if (!agent.hasPath)
+        {
+            return Color.blue;
+        }
+
+        switch (agent.pathStatus)
+        {
+            case NavMeshPathStatus.PathComplete: return Color.green;
+            case NavMeshPathStatus.PathInvalid: return Color.red;
+            case NavMeshPathStatus.PathPartial: return Color.yellow;
+        }
+
+        return Color.white;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = GetNavAgentStatusColor();
+            Gizmos.DrawSphere(CachedTransform.position + Vector3.up * 2f, 0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCar/ControlByMouse.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class ControlByMouse : MonoBehaviour
+{
+    private Camera mainCam;
+
+    public static Vector3 TargetPosition { get; private set; }
+    public static float LastTargetSetTimestamp { get; private set; }
+
+    public bool keepTargetVisible;
+    public float fadeOutDuration = 2f;
+
+    private RaycastHit[] singleHitBuffer = new RaycastHit[1];
+    private int terrainLayer;
+    private Ray raycastRay;
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+        terrainLayer = LayerMask.GetMask("Terrain");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetTargetPositionByMouseCoordinates(Input.mousePosition);
+        }
+    }
+
+    private void SetTargetPositionByMouseCoordinates(Vector3 mousePositionScreenCoords)
+    {
+        raycastRay = mainCam.ScreenPointToRay(mousePositionScreenCoords);
+        if (Physics.RaycastNonAlloc(raycastRay, singleHitBuffer, 500f, terrainLayer) > 0)
+        {
+            TargetPosition = singleHitBuffer[0].point;
+            LastTargetSetTimestamp = Time.time;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            float elapsed = Time.time - LastTargetSetTimestamp;
+
+            if (!keepTargetVisible && elapsed > fadeOutDuration)
+            {
+                return;
+            }
+
+            Color opacity = Color.white;
+            if (!keepTargetVisible)
+            {
+                float opacityAlpha = Mathf.Lerp(1f, 0f, elapsed / fadeOutDuration);
+                opacity.a = opacityAlpha;
+            }
+
+            Gizmos.color = Color.red * opacity;
+            Gizmos.DrawSphere(TargetPosition, 0.5f);
+        }
+    }
+}
